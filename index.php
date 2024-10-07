025fa62c7eeb60c9f399f2214b0b7d67d41bfccf
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-07 16:01:17 +0200

    added some basic style to the page

--- index.php
+++ index.php
@@ -2,17 +2,21 @@
 Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password
 (abbastanza) sicure.
 L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+
 Milestone 1
 Creare un form che invii in GET la lunghezza della password.
 Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere,
 lettere maiuscole, numeri e simboli) da restituire all’utente.
 Scriviamo tutto (logica e layout) in un unico file index.php
+
 Milestone 2
 Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che
 includeremo poi nella pagina principale.
+
 Milestone 3 (BONUS)
 Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite
 $_SESSION recupererà la password da mostrare all’utente.
+
 Milestone 4 (BONUS)
 Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli.
 Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e
@@ -34,10 +38,15 @@ Dare all’utente anche la possibilità di permettere o meno la ripetizione di c
     <!-- added bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/style.css">
 </head>
 <body>
     <main class="container">
+        <section class="text-center">
+            <h1 class="text-secondary pt-5">STRONG PASSWORD GENERATOR</h1>
+            <h2 class="text-white pb-3">Genera una password sicura</h2>
 
+        </section>
     </main>
 </body>
 </html>
